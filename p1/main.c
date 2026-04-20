9111f86ca0fb2b7203bc7a2da7c158d361f7b381
2025020142 <shleei20050927@gmail.com>
2026-04-20 15:38:51 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,20 +4,19 @@ int main(void)
 {
     int answer, guess, count = 0;
 
-    printf("정답 입력 : ");
     scanf("%d", &answer);
 
     do {
         scanf("%d", &guess);
         count++;
         if (guess > answer)
-            printf("%d 보다 낮습니다\n", guess);
+            printf("%d>?\n", guess);
         else if (guess < answer)
-            printf("%d 보다 높습니다\n", guess);
+            printf("%d<?\n", guess);
         else
-            printf("%d 정답입니다\n", guess);
+            printf("%d==?\n", guess);
     } while (guess != answer);
 
-    printf("시도횟수는 %d회\n", count);
+    printf("%d\n", count);
     return 0;
 }
